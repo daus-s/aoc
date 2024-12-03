abba58014ad1ceb6b60adb6e7da6c74e654bed9b
Daus Carmichael <daviscarmichael@chadlike.lan>
2024-12-03 00:40:43 -0800

    Day 2: really learned how to read today

--- 2024/haskell/app/Reports.hs
+++ 2024/haskell/app/Reports.hs
@@ -13,25 +13,57 @@ processFile filename = do
   contents <- readFile filename
   let rows = lines contents
   let reports = map readReport rows
-  let safe = countSafeEither reports
-  printf "is [7, 6, 4, 2, 1] safe? expect yes, received %s\n" (if safeDecline [7, 6, 4, 2, 1] then "yes" else "no")
-  printf "is [1, 2, 3, 4, 5] safe? expect yes, received %s\n" (if safeIncline [1, 2, 3, 4, 5] then "yes" else "no")
-  print safe
+  let safe = sum (map countSafeEither reports)
+  let maintained = sum (map withSafety reports)
+
+  printf "number of safe reports: %d\n" safe
+  printf "number of safe reports with backups: %d\n" maintained
 
 safeDecline :: [Int] -> Bool
-safeDecline (x : xs) = x <= head xs + 1 && x >= head xs - 2 && safeDecline xs
-safeDecline [_] = True
+safeDecline (f : s : rest) = f - 1 >= s && f - 3 <= s && safeDecline (s : rest)
+safeDecline (x : xs) = True --suspect
 safeDecline [] = True
 
 safeIncline :: [Int] -> Bool
-safeIncline (x : xs) = x > head xs && x <= head xs + 3 && safeDecline xs
+safeIncline (f : s : rest) = f + 1 <= s && f + 3 >= s && safeIncline (s : rest)
+safeIncline (x : xs) = True --suspect
 safeIncline [] = True
 
 readReport :: String -> [Int]
 readReport x = map read (words x)
 
-countSafeEither :: [[Int]] -> Int
-countSafeEither =
-  foldr
-    (\x -> (+) (if safeDecline x || safeIncline x then 1 else 0))
-    0
+countSafeEither :: [Int] -> Int
+countSafeEither x = if safeDecline x || safeIncline x then 1 else 0
+
+withoutOne :: [Int] -> [[Int]]
+withoutOne xs = map (removeIndex xs) [0 .. (length xs - 1)]
+
+removeIndex :: [Int] -> Int -> [Int] -- this was the suspect
+-- # learn how to read this
+{-
+  removeIndex, given:
+    list
+    index
+  is defined as
+    concatenate given
+      left
+      right
+  from
+    split the list given:
+        index
+      is defined as
+        ( [left), [right] )
+
+  (a) removeIndex, given a list and index is defined as
+  (b) split the list at the index (with the left exclusive and the right inclusive),
+  (c) then concatenate the lists given the tail of the right list and all of the left list
+
+  aside: a proof that c is correct is left to the reader as an exercise
+-}
+removeIndex xs n = let (ys, zs) = splitAt n xs in ys ++ drop 1 zs
+
+checkSafety :: [Int] -> Bool
+checkSafety x = safeDecline x || safeIncline x
+
+withSafety :: [Int] -> Int
+withSafety x = if any checkSafety (withoutOne x) then 1 else 0
