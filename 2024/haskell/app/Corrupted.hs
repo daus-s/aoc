2c43b19bf5a4434ebea00aed4c951d1aa3fe8a65
daus-s <dauscarmichael@gmail.com>
2024-12-04 00:04:24 -0800

    Day 3: make sure to make ur recursive function call itself
    squash pls

--- 2024/haskell/app/Corrupted.hs
+++ 2024/haskell/app/Corrupted.hs
@@ -1,5 +1,5 @@
 import Data.List (isPrefixOf, stripPrefix)
-import Data.Strings (Str (strSplit, strSplitAll))
+import Data.Strings (Str (strBreak, strJoin, strSplit, strSplitAll))
 import System.Environment (getArgs)
 import Text.Printf (printf)
 import Text.Read (readMaybe)
@@ -18,6 +18,11 @@ processFile filename = do
   let products = sum . map tryParseNum $ instructionsToOperands $ contents
   printf "the product of the valid multiply instructions is: %d\n" products
 
+  printf "enabled flow = %s\n\n" (drool contents)
+
+  let controlProds = sum . map tryParseNum $ instructionsToOperands $ drool contents
+  printf "the product of the valid multiply instructions with proper control flow: %d\n" controlProds
+
 -- transform text to code
 instructionsToOperands :: String -> [String]
 instructionsToOperands x = strSplitAll "mul(" x
@@ -34,5 +39,10 @@ readTwo (left, right) = case (readInt left, readInt right) of
   (Nothing, Just r) -> Nothing
   (_, _) -> Nothing
 
+-- one-liner inshallah
 readInt :: String -> Maybe Int
 readInt x = readMaybe x
+
+drool :: String -> String
+drool "" = "" -- base case that is specifc goes first, perchance more specificity means higher
+drool string = (fst (strBreak "don't()" string)) ++ drool (snd (strBreak "do()" (snd (strBreak "don't()" string))))
